fa1d17a435a4dd7587e2c9c34a8e4aa0ab2ea8b2
FrancescaLai <francescalai1690@gmail.com>
2021-04-07 17:15:14 +0200

    Last step done = list with 10 emails

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ var app = new Vue(
   {
     el: "#app",
     data: {
+      email: "",
       emails: [],
     },
     mounted: function(){
 
-      var self = this;
-
       for (var i = 0; i < 10; i++) {
+
+        var self = this;
 
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 
