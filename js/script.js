6bc6f26445a417caeafdcbee4c481eaf9a63e2ee
FrancescaLai <francescalai1690@gmail.com>
2021-04-07 17:04:35 +0200

    10 emails step done

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ var app = new Vue(
   {
     el: "#app",
     data: {
-      email: "francesca@boolean.com",
+      emails: [],
     },
     mounted: function(){
+
       var self = this;
 
-      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then(function ( risposta ){
-        self.email = risposta.data.response;
-      });
+      for (var i = 0; i < 10; i++) {
+
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+        .then(function ( risposta ){
+          self.emails.push(risposta.data.response);
+        });
+      }
     }
   }
 );
